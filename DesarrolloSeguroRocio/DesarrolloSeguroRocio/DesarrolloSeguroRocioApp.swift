cd2d63a60f093b9d31c62232717a5aa1d7654077
Rocío Martos <rocionintendo29@gmail.com>
2024-05-18 16:09:26 +0200

    Empezamos el proyecto

---
+++ DesarrolloSeguroRocio/DesarrolloSeguroRocio/DesarrolloSeguroRocioApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesarrolloSeguroRocioApp.swift
+//  DesarrolloSeguroRocio
+//
+//  Created by Rocio Martos on 18/5/24.
+//
+
+import SwiftUI
+
+@main
+struct DesarrolloSeguroRocioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
